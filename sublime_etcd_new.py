a72d76fbe0c95403cb0296e1cf9a148bd8d7961c
Shay Erlichmen <erlichmen@gmail.com>
2014-10-12 09:55:50 +0300

    first release

---
+++ sublime_etcd_new.py
@@ -0,0 +1,9 @@
+import sublime, sublime_plugin
+ 
+class etcdNewCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+    	import urllib2
+    	response = urllib2.urlopen('https://discovery.etcd.io/new')
+		etcd_token = response.read()
+
+        self.view.insert(edit, 0, etcd_token)
